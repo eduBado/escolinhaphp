e6cb788e9a92c74a748371a9986b9ccb116efa56
eduBado <edubado08@gmail.com>
2015-11-19 03:39:48 -0200

    Criação do arquivo PHP

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Manuseio de Formulário com PHP</h1>
+        <p>Referências</p>
+        <ul>
+            <li><a href="http://www.w3schools.com/">W3Schools</a></li>
+            <li><a href="http://www.w3schools.com/php/">Manual do PHP</a></li>
+        </ul>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
