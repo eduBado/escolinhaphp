da4e809af9efb532d977b18c1597394e08413e25
eduBado <edubado08@gmail.com>
2015-11-19 05:48:58 -0200

    Envia POST e GET

--- index.php
+++ index.php
@@ -1,8 +1,66 @@
+<?php
+// define variables and set to empty values
+$name = $email = $gender = $comment = $website = "";
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["name"])) {
+        $nameErr = "Name is required";
+    } else {
+        $name = test_input($_POST["name"]);
+        if (!preg_match("/^[a-zA-Z ]*$/", $name)) {
+            $nameErr = "Only letters and white space allowed";
+        }
+    }
+
+    if (empty($_POST["email"])) {
+        $emailErr = "Email is required";
+    } else {
+        $email = test_input($_POST["email"]);
+    }
+
+    if (empty($_POST["website"])) {
+        $website = "";
+    } else {
+        $website = test_input($_POST["website"]);
+    }
+
+    if (empty($_POST["comment"])) {
+        $comment = "";
+    } else {
+        $comment = test_input($_POST["comment"]);
+    }
+
+    if (empty($_POST["gender"])) {
+        $genderErr = "Gender is required";
+    } else {
+        $gender = test_input($_POST["gender"]);
+    }
+}
+
+//Limpeza dos dados de entrada
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
+
+
+
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <style>
+            .err{
+                outline: 1px dashed red;
+                background-color: rgba (255,0,0,0.2);
+            }
+        </style>
     </head>
     <body>
         <h1>Manuseio de Formulário com PHP</h1>
@@ -11,6 +69,29 @@
             <li><a href="http://www.w3schools.com/">W3Schools</a></li>
             <li><a href="http://www.w3schools.com/php/">Manual do PHP</a></li>
         </ul>
-        
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+
+            Name: <input type="text" name="name" class="<?= strlen($nameErr) != 0 ? "err" : ""; ?>">
+            <span class="error">* <?php echo $nameErr; ?></span>
+            <br><br>
+            E-mail:
+            <input type="text" name="email" class="<?= strlen($emailErr) != 0 ? "err" : ""; ?>">
+            <span class="error">* <?php echo @$emailErr; ?></span>
+            <br><br>
+            Website:
+            <input type="text" name="website" >
+            <span class="error"><?php echo @$websiteErr; ?></span>
+            <br><br>
+            Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+            <br><br>
+            Gender:
+            <input type="radio" name="gender" value="female" class="<?= strlen($genderErr) != 0 ? "err" : ""; ?>">Female
+            <input type="radio" name="gender" value="male" class="<?= strlen($genderErr) != 0 ? "err" : ""; ?>">Male
+            <span class="error">* <?php echo @$genderErr; ?></span>
+            <br><br>
+            <input type="submit" name="submit" value="Submit"> 
+
+        </form>
+
     </body>
 </html>
